787db2affe808d2646ea31befe2d71718cf90095
UlysseDev <ulysse.curier@gmail.com>
2026-03-16 14:30:18 +0100

    fix: prevent duplicate definitions

--- src/common/jeton.h
+++ src/common/jeton.h
@@ -1,39 +1,58 @@
-//Définition des différents tokens utilisés lors du projet
+// Définition des différents tokens utilisés lors du projet
 
 typedef enum {
-REEL,OPERATEUR,FONCTION,ERREUR,FIN,PAR_OUV,PAR_FEM,PAR_FERM,VARIABLE,BAR_OUV,BAR_FERM,ABSOLU
-}typetoken;
+  REEL,
+  OPERATEUR,
+  FONCTION,
+  ERREUR,
+  FIN,
+  PAR_OUV,
+  PAR_FEM,
+  PAR_FERM,
+  VARIABLE,
+  BAR_OUV,
+  BAR_FERM,
+  ABSOLU
+} typetoken;
+
+typedef enum { PLUS, MOINS, FOIS, DIV, PUIS } typeoperateur;
 
 typedef enum {
-    PLUS,MOINS,FOIS,DIV,PUIS
-}typeoperateur;
+  ABS,
+  SIN,
+  SQRT,
+  LOG,
+  COS,
+  TAN,
+  EXP,
+  ENTIER,
+  VAL_NEG,
+  SINC
+} typefonction;
 
 typedef enum {
-    ABS,SIN,SQRT,LOG,COS,TAN,EXP,ENTIER,VAL_NEG,SINC
-}typefonction;
-
-typedef enum{
-        DIVISER_PAR_ZERO,PAR_OUV,PAR_FERM,FUN_NO_RECO,VAR_NO_RECO,ARG_MANQUANT
-}typeerreur;
+  DIVISER_PAR_ZERO,
+  FUN_NO_RECO,
+  VAR_NO_RECO,
+  ARG_MANQUANT
+} typeerreur;
 
 typedef union {
-    float reel;
-    typefonction fonction;
-    typeoperateur operateur;
-    typeerreur erreur;
-}typevaleur;
+  float reel;
+  typefonction fonction;
+  typeoperateur operateur;
+  typeerreur erreur;
+} typevaleur;
 
-typedef struct{
-    typetoken lexem;
-    typevaleur valeur;
-}typejeton;
+typedef struct {
+  typetoken lexem;
+  typevaleur valeur;
+} typejeton;
 
-typedef struct Node{
-    typejeton jeton;
-    struct Node *pjeton_preced;
-    struct Node *pjeton_suiv;
-}Node;
+typedef struct Node {
+  typejeton jeton;
+  struct Node *pjeton_preced;
+  struct Node *pjeton_suiv;
+} Node;
 
 typedef Node *Arbre;
-
-
